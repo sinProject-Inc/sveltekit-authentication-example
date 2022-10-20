0df5f3a245a01c66af2cc3cc53c93f69b635e4e0
sinYa Iwasaki <iwasaki@sinproject.net>
2022-10-20 15:21:01 +0900

    ユーザー登録ページを作成する #5

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,43 @@
+import { db } from '$lib/database'
+import { invalid, redirect, type Actions } from '@sveltejs/kit'
+import bcrypt from 'bcrypt'
+import type { PageServerLoad } from './$types'
+
+enum Roles {
+	admin = 'admin',
+	user = 'user',
+}
+
+export const load: PageServerLoad = async () => {
+	// todo
+}
+
+export const actions: Actions = {
+	default: async ({ request }) => {
+		const data = await request.formData()
+		const username = data.get('username') as string
+		const email = data.get('email') as string
+		const password = data.get('password') as string
+
+		if (!username || !email || !password) return invalid(404, { missing: true, username, email })
+
+		try {
+			await db.user.create({
+				data: {
+					username,
+					email,
+					password: await bcrypt.hash(password, 10),
+					role: { connect: { name: Roles.user } },
+				},
+			})
+		} catch (error) {
+			console.error(error)
+			return invalid(400, { user_exists: true, username, email })
+		}
+
+		console.log('aaa')
+
+		throw redirect(303, '/login')
+	},
+}
+
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { ActionData } from './$types';
+
+  export let form: ActionData;
+</script>
+
+<h1>Register</h1>
+
+<form method="POST">
+	<input type="text" name="username" placeholder="Username" required />
+	<input type="email" name="email" placeholder="email" required />
+	<input type="password" name="password" placeholder="Password" required />
+
+	{#if form?.missing}<p class="error">Username, email and password are required.</p>{/if}
+	{#if form?.user_exists}<p class="error">Username or email is used.</p>{/if}
+
+	<input type="submit" value="Register" />
+</form>
