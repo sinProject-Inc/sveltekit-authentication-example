0200a711bf35b363fe6567a5416bb63d314220f0
sinYa Iwasaki <iwasaki@sinproject.net>
2022-10-21 22:35:01 +0900

    ログイン判定を行い、ログインページにリダイレクトする #15

---
+++ src/routes/main/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+</script>
+
+<svelte:head>
+	<title>Main page</title>
+</svelte:head>
+
+{#if $page.data.user}
+	<h1>Welcome {$page.data.user.username}</h1>
+	Role: {$page.data.user.role}
+{:else}
+	<h1>Welcome</h1>
+{/if}
