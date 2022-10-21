041e04ed0544110badebc6c73eca2151443c438a
sinYa Iwasaki <iwasaki@sinproject.net>
2022-10-21 15:55:37 +0900

    event.locals の interface を定義する #13

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,7 +2,12 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		user: {
+			username: string
+			role: string
+		}
+	}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
